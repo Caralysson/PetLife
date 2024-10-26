ec1009caf57add9bd71a5e6020ab2c1d88d7a000
Caralysson <alyssonsandrodalapicula@gmail.com>
2024-10-26 20:15:35 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ elementosDuvidas.forEach (function (duvida) {
    duvida.addEventListener ('click', function () {
      elementosDuvidas.forEach(function (item) {
       item.classList.remove('ativa');
-     })
-})
+     });
+     duvida.classList.toggle('ativa');
+  })
 })
 
 //function nome(argumento) {}
+// teste isso aqui rapaz =========================
+// =========================================
+// ===============================================
