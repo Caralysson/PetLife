d55874e3d2fc52b64b0145580546dafc9bb930a6
Caralysson <alyssonsandrodalapicula@gmail.com>
2024-10-26 20:41:53 -0300

    Concertado

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
+const elementosDuvidas = document.querySelectorAll('.duvida');
 
- var elementosDuvidas = document.querySelectorAll('.duvida')
-
-elementosDuvidas.forEach (function (duvida) {
-   duvida.addEventListener ('click', function () {
-     elementosDuvidas.forEach(function (item) {
-      item.classList.remove('ativa');
-     });
-     duvida.classList.toggle('ativa');
-  })
-})
-
-//function nome(argumento) {}
-// teste isso aqui rapaz =========================
-// =========================================
-// ===============================================
+elementosDuvidas.forEach((duvida) => {
+  duvida.addEventListener('click', () => {
+    duvida.classList.toggle('ativa');
+  });
+});
